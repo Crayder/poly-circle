222bce383c73012e369add16dc3dc1e4c0d0084c
Crayder <crayder@email.com>
2025-01-09 15:38:05 -0500

    Prevent 0 width/height rects

--- poly-circle-from-db/poly.py
+++ poly-circle-from-db/poly.py
@@ -97,6 +97,10 @@ def convert_polygon_to_blueprint(grid_points, center_x, center_y):
         point_a = edge["point_a"]
         point_b = edge["point_b"]
 
+        # If 0 width/height skip this edge
+        if point_a.x == point_b.x or point_a.y == point_b.y:
+            continue
+
         rect = {
             "x": point_a.x,
             "y": point_a.y,
